2926c2bbb85a772515df9ccd0a697b79aa65c343
Szymon Świergosz <szymonswiergosz@MacBook-Pro-Szymon.local>
2021-09-20 11:55:09 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vue-typescript-basics
-Basic app for learning Vue w/ Typescript
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import Job from './types/Job';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: 'Farm worker',
+        location: 'Warsaw',
+        salary: 1200,
+        id: '1',
+      },
+      {
+        title: 'Fisherman',
+        location: 'Somewhere over the rainbow',
+        salary: 1500,
+        id: '2',
+      }
+    ])
+
+    return { jobs }
+  },
+});
+</script>
+
+<style>
+</style>
